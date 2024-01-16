0fe5e5e3295d8ba0fffced8467dab5583cc4d82c
David <davo00771339@gmail.com>
2024-01-16 15:18:01 +0400

    Ready BookingFormPage Added

--- src/components/utils.js
+++
@@ -1,7 +0,0 @@
-// utils.js
-export const calculatePrice = (startDate, endDate) => {
-    const baseRate = 20;
-    const numberOfDays = Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24)) + 1;
-    return baseRate * numberOfDays;
-  };
-  
